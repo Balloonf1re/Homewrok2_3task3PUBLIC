2f7ab4c08e33f26498a67457751e05ffffa62c3c
Balloonfire <balloonfire.yt@gmail.com>
2024-01-04 14:49:31 +0200

    Commit private

---
+++ src/Main2.java
@@ -0,0 +1,9 @@
+public class Main2 {
+    public static void main (String[] args) {
+        double pi = Math.PI;
+        double e = Math.E;
+        System.out.println(pi);
+        System.out.println(e);
+//Чило e з домашки є завеликим для команди double, тому двійка в кінці скорочується.
+    }
+}
